1432dfd88cb918b555b1fa8547d8b7add2066ae1
adeyemi gbenga <addegbenga@gmail.com>
2019-10-13 01:03:53 +0100

    name

--- add.js
+++ add.js
@@ -1,7 +1,12 @@
+ const bttn1 = document.querySelector('.bttn1');
+ const bttn2 = document.querySelector('.bttn2');
+ const bttn3 = document.querySelector('.bttn3');
+
  var modalBg = document.querySelector(".modal-bg");
  var modalclose = document.querySelector(".modal-close");
  document.querySelector('.modal-btn').addEventListener('click', function() {
      modalBg.classList.add('modal-active');
+
 
  });
  modalclose.addEventListener('click', function() {
@@ -9,8 +14,11 @@
  });
  var aboutmeinfo = document.querySelector('.aboutmeinfo');
 
+
  document.querySelector('.bttn1').addEventListener('click', function() {
      aboutmeinfo.classList.toggle('profile-active')
+     bttn1.classList.toggle('bttn-active')
+
      if (aboutmeinfo.style.maxHeight) {
          aboutmeinfo.style.maxHeight = null
 
@@ -24,6 +32,7 @@
 
  document.querySelector('.bttn2').addEventListener('click', function() {
      contactme.classList.toggle('profile-active')
+     bttn2.classList.toggle('bttn-active')
      if (contactme.style.maxHeight) {
          contactme.style.maxHeight = null
 
@@ -37,6 +46,7 @@
 
  document.querySelector('.bttn3').addEventListener('click', function() {
      socialme.classList.toggle('profile-active')
+     bttn3.classList.toggle('bttn-active')
      if (socialme.style.maxHeight) {
          socialme.style.maxHeight = null
 
